eb5bbe3c4b06e1434d49a21f87bb9a94b42f00ef
Miguel Angel Perez Franco <miguel.perezf@upb.edu.co>
2020-10-06 15:06:36 -0500

    Prueba

--- Hotel/Persona/Persona.cs
+++ Hotel/Persona/Persona.cs
@@ -7,6 +7,7 @@ namespace Hotel
     public class Persona 
     {
         //este es un comentario que hizo Nicole el 6/10/2020 a las 2:57pm 
+        //Prueba
 
         //atributos
         private string nombre; //es ingresado por el usuario
