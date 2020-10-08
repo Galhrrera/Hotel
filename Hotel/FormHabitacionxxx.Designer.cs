5c0e474dc9bcf57678b591cb6576bfa81970965f
Juan Esteban Galeano Herrera <juan.galeanoh@upb.edu.co>
2020-10-08 15:56:08 -0500

    Se agregó el formulario "Habitación XXX" Falta poder abrirlo tras hacer click en alguna habitación de la lista de habitaciones del formulario Habitciones.

---
+++ Hotel/FormHabitacionxxx.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Hotel
+{
+    partial class FormHabitacionxxx
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHabitacion = new System.Windows.Forms.Label();
+            this.lblNumHabitacion = new System.Windows.Forms.Label();
+            this.lblTipoHabitacion = new System.Windows.Forms.Label();
+            this.lblHuesped = new System.Windows.Forms.Label();
+            this.lblTipoCama = new System.Windows.Forms.Label();
+            this.btnServicios = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lblSubTotalProductos = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblHabitacion
+            // 
+            this.lblHabitacion.AutoSize = true;
+            this.lblHabitacion.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHabitacion.Location = new System.Drawing.Point(298, 30);
+            this.lblHabitacion.Name = "lblHabitacion";
+            this.lblHabitacion.Size = new System.Drawing.Size(109, 20);
+            this.lblHabitacion.TabIndex = 0;
+            this.lblHabitacion.Text = "Habitación: ";
+            // 
+            // lblNumHabitacion
+            // 
+            this.lblNumHabitacion.AutoSize = true;
+            this.lblNumHabitacion.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumHabitacion.Location = new System.Drawing.Point(413, 30);
+            this.lblNumHabitacion.Name = "lblNumHabitacion";
+            this.lblNumHabitacion.Size = new System.Drawing.Size(42, 20);
+            this.lblNumHabitacion.TabIndex = 1;
+            this.lblNumHabitacion.Text = "XXX";
+            // 
+            // lblTipoHabitacion
+            // 
+            this.lblTipoHabitacion.AutoSize = true;
+            this.lblTipoHabitacion.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoHabitacion.Location = new System.Drawing.Point(38, 101);
+            this.lblTipoHabitacion.Name = "lblTipoHabitacion";
+            this.lblTipoHabitacion.Size = new System.Drawing.Size(47, 21);
+            this.lblTipoHabitacion.TabIndex = 2;
+            this.lblTipoHabitacion.Text = "Tipo: ";
+            // 
+            // lblHuesped
+            // 
+            this.lblHuesped.AutoSize = true;
+            this.lblHuesped.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHuesped.Location = new System.Drawing.Point(38, 140);
+            this.lblHuesped.Name = "lblHuesped";
+            this.lblHuesped.Size = new System.Drawing.Size(93, 20);
+            this.lblHuesped.TabIndex = 3;
+            this.lblHuesped.Text = "Huesped: ";
+            // 
+            // lblTipoCama
+            // 
+            this.lblTipoCama.AutoSize = true;
+            this.lblTipoCama.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoCama.Location = new System.Drawing.Point(38, 186);
+            this.lblTipoCama.Name = "lblTipoCama";
+            this.lblTipoCama.Size = new System.Drawing.Size(132, 20);
+            this.lblTipoCama.TabIndex = 4;
+            this.lblTipoCama.Text = "Tipo de cama: ";
+            // 
+            // btnServicios
+            // 
+            this.btnServicios.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnServicios.Location = new System.Drawing.Point(42, 270);
+            this.btnServicios.Name = "btnServicios";
+            this.btnServicios.Size = new System.Drawing.Size(128, 32);
+            this.btnServicios.TabIndex = 5;
+            this.btnServicios.Text = "Servicios";
+            this.btnServicios.UseVisualStyleBackColor = true;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar.Location = new System.Drawing.Point(225, 270);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(128, 32);
+            this.btnRegresar.TabIndex = 6;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(524, 101);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(225, 101);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(250, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(225, 140);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(250, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(225, 186);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(250, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // lblSubTotalProductos
+            // 
+            this.lblSubTotalProductos.AutoSize = true;
+            this.lblSubTotalProductos.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSubTotalProductos.Location = new System.Drawing.Point(524, 270);
+            this.lblSubTotalProductos.Name = "lblSubTotalProductos";
+            this.lblSubTotalProductos.Size = new System.Drawing.Size(91, 20);
+            this.lblSubTotalProductos.TabIndex = 11;
+            this.lblSubTotalProductos.Text = "Subtotal: ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(634, 270);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(130, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // FormHabitacionxxx
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 349);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.lblSubTotalProductos);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnServicios);
+            this.Controls.Add(this.lblTipoCama);
+            this.Controls.Add(this.lblHuesped);
+            this.Controls.Add(this.lblTipoHabitacion);
+            this.Controls.Add(this.lblNumHabitacion);
+            this.Controls.Add(this.lblHabitacion);
+            this.Name = "FormHabitacionxxx";
+            this.Text = "FormHabitacionxxx";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHabitacion;
+        private System.Windows.Forms.Label lblNumHabitacion;
+        private System.Windows.Forms.Label lblTipoHabitacion;
+        private System.Windows.Forms.Label lblHuesped;
+        private System.Windows.Forms.Label lblTipoCama;
+        private System.Windows.Forms.Button btnServicios;
+        private System.Windows.Forms.Button btnRegresar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lblSubTotalProductos;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
--- Hotel/Reserva/Reserva.cs
+++ Hotel/Reserva/Reserva.cs
@@ -8,8 +8,8 @@ namespace Hotel
     public class reserva
     {        
         private Persona persona;
-        private int numReserva;                                                
-        private Habitacion.estado estado;        
+        private int numReserva;
+        private Habitacion Habreservada;      
         private Random rnd = new Random(DateTime.Now.Millisecond); //Generador de número de reserva
         private List<HabitacionReservada> listaHabitaciones;       //Se necesita la clase HabitacionReservada
                                                                    //Clase de asociación entre reserva y habitación
@@ -18,6 +18,7 @@ namespace Hotel
         
         public List<HabitacionReservada> ListaHabitaciones { get => listaHabitaciones; }
         public int NumReserva { get => numReserva; set => numReserva = value; }
+        public Habitacion Habreservada1 { get => Habreservada; set => Habreservada = value; }
 
         public reserva(Persona cliente)
         {
@@ -27,7 +28,7 @@ namespace Hotel
         public void HacerCheckIn()
         {
             this.NumReserva = rnd.Next(111111, 999999);
-            estado = Habitacion.estado.Ocupada;
+            //estado = Habitacion.estado.Ocupada;
         }
 
         public void HacerCheckOut()
